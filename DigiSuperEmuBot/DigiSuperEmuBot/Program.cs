2bdf89207d6c119568e3bd1728f9f1301e790a3d
Benjamin Kohler <bkohler616@hotmail.com>
2016-07-11 23:28:41 -0400

    Initial commit with some basic stuff.

---
+++ DigiSuperEmuBot/DigiSuperEmuBot/Program.cs
@@ -0,0 +1,32 @@
+﻿using Discord;
+using Discord.Commands;
+
+namespace DigiSuperEmuBot
+{
+    class Program
+    {
+        static void Main(string[] args) => new Program().Start();
+        private static DiscordClient _client;
+
+        public void Start()
+        {
+            _client = new DiscordClient();
+            SetupCommands();
+            _client.ExecuteAndWait(async () => {
+                await _client.Connect("MjAxNDg5MzQ1NDgyNDU3MDg4.CmNHTA.vdLHCu7IXu9fnycTj9uKECX2F2Y");
+            });
+        }
+
+        public void SetupCommands()
+        {
+            _client.UsingCommands(x =>
+            {
+                x.PrefixChar = '~';
+                x.HelpMode = HelpMode.Disabled;
+            });
+
+            var rolling = new CommandModuleGambling.Rolling();
+            rolling.Setup(_client);
+        }
+    }
+}
